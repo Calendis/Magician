a8fb0cea0bc633fad7a50fab3d45bba76e602d39
Calendis <calendis1@gmail.com>
2022-09-22 20:12:50 -0600

    Anything "pollable" like a function implements map

---
+++ magician/Map.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magician
+{
+    public interface Map
+    {
+        public double evaluate(double x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
