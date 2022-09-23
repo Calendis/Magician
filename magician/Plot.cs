c047985af300d42699a97bfa20471e3fea5c3c66
Calendis <calendis1@gmail.com>
2022-09-22 20:11:41 -0600

    Starting porting FuncPlot from MindEye to Plot

---
+++ magician/Plot.cs
@@ -0,0 +1,17 @@
+namespace Magician
+{
+    public class Plot : Multi, Map
+    {
+        Driver toPlot;
+
+        public Plot()
+        {
+            //
+        }   
+        public Line interpolate(double x, double dx)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
