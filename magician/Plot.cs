b0959aad4ca11ace6efef6607638989c8d7802bc
Calendis <calendis1@gmail.com>
2022-09-22 22:10:06 -0600

    Plot now hooks up to the Driver system

--- magician/Plot.cs
+++ magician/Plot.cs
@@ -2,12 +2,29 @@ namespace Magician
 {
     public class Plot : Multi, Map
     {
-        Driver toPlot;
+        protected Driver toPlot;
+        private double start;
+        private double end;
 
+        public Plot(Driver d, double start, double end)
+        {
+            toPlot = d;
+            this.start = start;
+            this.end = end;
+        }
+
+        // Default plot of nothing
         public Plot()
         {
-            //
-        }   
+            toPlot = new Driver();
+            start = 0;
+            end = 0;
+        }
+
+        public double Evaluate(params double[] x)
+        {
+            return toPlot.Evaluate(x);
+        }
         public Line interpolate(double x, double dx)
         {
             throw new NotImplementedException();
