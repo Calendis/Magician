ce81c59b5dac697a7b24cc5cab248e2f1563d099
Calendis <calendis1@gmail.com>
2022-09-22 23:16:01 -0600

    Fixed color bitmath

--- magician/Color.cs
+++ magician/Color.cs
@@ -1,11 +1,13 @@
 namespace Magician
 {
-    class Color
+    public class Color
     {
         // 32-bit int representing
-        private int col;
-        public char R => (char)(col & 0xff000000);
-        public Color(char r, char g, char b, int a, bool hsv=false)
+        private uint col;
+        public byte R => (byte)((col & 0xff000000) >> 24);
+        public byte G => (byte)((col & 0x00ff0000) >> 16);
+        public byte B => (byte)((col & 0x0000ff00) >> 8);
+        public Color(byte r, byte g, byte b, int a, bool hsv=false)
         {
             // Convert to HSV before setting
             if (hsv)
@@ -15,44 +17,48 @@ namespace Magician
                 int v;
             }
 
-            char byte1 = (char)(r & 0xff000000);
-            char byte2 = (char)((g & 0xff000000) >> 8);
-            char byte3 = (char)((r & 0xff000000) >> 16);
+            Console.WriteLine($"RGB: {r}, {g}, {b}");
 
-            col = byte1 + byte2 + byte3;
+            col = (uint)(r << 24) + (uint)(g << 16) + (uint)(b << 8);
         }
 
-        public Color(int hex, bool hsv=false)
+        public Color(uint hex, bool hsv=false)
         {
             if (hsv)
             {
                 // do hsv conversion
             }
+            col = hex;
         }
 
-        public static int RGBToHSV(char r, char g, char b)
+        /*public Color()
+        {
+            col = 0x00ff00ff;
+        }*/
+
+        public static int RGBToHSV(byte r, byte g, byte b)
         {
             throw new NotImplementedException();
         }
 
         public static int RGBToHSV(int hex)
         {
-            char r = (char)(hex & 0xff000000);
-            char g = (char)(hex & 0x00ff0000);
-            char b = (char)(hex & 0x0000ff00);
+            byte r = (byte)(hex & 0xff000000);
+            byte g = (byte)(hex & 0x00ff0000);
+            byte b = (byte)(hex & 0x0000ff00);
             return RGBToHSV(r, g, b);
         }
 
-        public static int HSVToRGB(char h, char s, char v)
+        public static int HSVToRGB(byte h, byte s, byte v)
         {
             throw new NotImplementedException();
         }
 
         public static int HSVToRGB(int hex)
         {
-            char h = (char)(hex & 0xff000000);
-            char s = (char)(hex & 0x00ff0000);
-            char v = (char)(hex & 0x000000ff);
+            byte h = (byte)(hex & 0xff000000);
+            byte s = (byte)(hex & 0x00ff0000);
+            byte v = (byte)(hex & 0x000000ff);
             return HSVToRGB(h, s, v);
         }
 
