3350d4f8cc99d64adb1e771844f46778d1096175
Calendis <calendis1@gmail.com>
2022-09-22 20:13:12 -0600

    Started porting Line from MindEye

---
+++ magician/Line.cs
@@ -0,0 +1,14 @@
+/*
+    A line is defined by two points, and extends infinitely in either
+*/
+
+namespace Magician
+{
+    public class Line : Plot
+    {
+        public Line(Point p0, Point p1)
+        {
+            //
+        }
+    }
+}
