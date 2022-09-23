29da1669dbd0b334c5fef5eace3dedbd877cbac4
Calendis <calendis1@gmail.com>
2022-09-22 22:11:10 -0600

    Added default driver

--- magician/Driver.cs
+++ magician/Driver.cs
@@ -6,14 +6,27 @@
 namespace Magician
 {
     delegate double DriveFunction(params double[] x);
-    class Driver : Map
+    public class Driver : Map
     {
         private DriveFunction driveFunction;
         private int ins;
+        
         public Driver(int ins, Func<double[], double> df)
         {
             this.ins = ins;
             driveFunction = new DriveFunction(df);
+        }
+
+        // Default "zero-driver"
+        public Driver()
+        {
+            ins = 0;
+            driveFunction = new DriveFunction(x => 0);
+        }
+
+        public double Evaluate(params double[] x)
+        {
+            return driveFunction(x);
         }
     }
 }
