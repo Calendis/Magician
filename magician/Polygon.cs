784f5631d28475da2fc77e878f5b93b631915d9b
Calendis <calendis1@gmail.com>
2022-09-25 03:55:21 -0600

    fixed relative drawing

--- magician/Polygon.cs
+++ magician/Polygon.cs
@@ -23,16 +23,16 @@ namespace Magician
                 Point p1 = (Point)constituents[i+1];
                 SDL_SetRenderDrawColor(renderer, p0.Col.R, p0.Col.G, p0.Col.B, 255);
                 SDL_RenderDrawLine(renderer,
-                (int)p0.XCartesian(xOffset), (int)p0.YCartesian(yOffset),
-                (int)p1.XCartesian(xOffset), (int)p1.YCartesian(yOffset));
+                (int)p0.XCartesian(pos[0]+xOffset), (int)p0.YCartesian(pos[1]+yOffset),
+                (int)p1.XCartesian(pos[0]+xOffset), (int)p1.YCartesian(pos[1]+yOffset));
             }
             
             Point pLast = (Point)constituents[constituents.Count-1];
             Point pFirst = (Point)constituents[0];
             SDL_SetRenderDrawColor(renderer, pLast.Col.R, pLast.Col.G, pLast.Col.B, 255);
             SDL_RenderDrawLine(renderer,
-            (int)pLast.XCartesian(xOffset), (int)pLast.YCartesian(yOffset),
-            (int)pFirst.XCartesian(xOffset), (int)pFirst.YCartesian(yOffset));
+            (int)pLast.XCartesian(pos[0]+xOffset), (int)pLast.YCartesian(pos[1]+yOffset),
+            (int)pFirst.XCartesian(pos[0]+xOffset), (int)pFirst.YCartesian(pos[1]+yOffset));
         }
 
         public void SetCol(Color c)
@@ -61,6 +61,22 @@ namespace Magician
             
             // Reset the order
             constituents = constituentsCopy;
+        }
+    }
+
+    public class RegularPolygon : Polygon
+    {
+        public RegularPolygon(int sides, double magnitude)
+        {
+            List<Point> ps = new List<Point>();
+            double angle = 360d / (double)sides;
+            for (int i = 0; i < sides; i++)
+            {
+                double x = magnitude*Math.Cos(angle*i/180*Math.PI);
+                double y = magnitude*Math.Sin(angle*i/180*Math.PI);
+                ps.Add(new Point(x, y));
+            }
+            constituents.AddRange(ps);
         }
     }
 }
