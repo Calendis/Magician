97f39f83449d0b1eced33cd84ef9a4f4cd3280ef
Calendis <calendis1@gmail.com>
2022-09-27 15:22:45 -0600

    changed window size

--- magician/Globals.cs
+++ magician/Globals.cs
@@ -2,8 +2,8 @@ namespace Magician
 {
     public static class Globals
     {
-        public static int winWidth = 800;
-        public static int winHeight = 600;
+        public static int winWidth = 1200;
+        public static int winHeight = 800;
 
         public static Color bgCol = new Color(0x001010ff);
         public static Color fgCol = new Color(0x00e9f5ff);
