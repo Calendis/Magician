ae7507d9b0649b78835439f5b3d7583b8ea2fb42
Calendis <calendis1@gmail.com>
2022-10-03 15:00:41 -0600

    partial hsv support (hue only)

--- magician/Color.cs
+++ magician/Color.cs
@@ -4,31 +4,70 @@ namespace Magician
     {
         // 32-bit int representing
         private uint col;
-        public uint HexCol => col;
-        public byte R => (byte)((col & 0xff000000) >> 24);
-        public byte G => (byte)((col & 0x00ff0000) >> 16);
-        public byte B => (byte)((col & 0x0000ff00) >> 8);
-        public byte A => (byte)((col & 0x000000ff) >> 0);
-        public Color(byte r, byte g, byte b, int a, bool hsv=false)
+        public uint HexCol
         {
-            // Convert to HSV before setting
-            if (hsv)
+            get => col;
+            set
             {
-                int h;
-                int s;
-                int v;
+                col = value;
+            }
+        }
+        public byte R
+        {
+            get => (byte)((col & 0xff000000) >> 24);
+            set
+            {
+                col = (uint)((byte)value << 24) + (col & 0x00ffffff);
             }
 
-            col = (uint)(r << 24) + (uint)(g << 16) + (uint)(b << 8);
+        } 
+        public byte G
+        {
+            get => (byte)((col & 0x00ff0000) >> 16);
+            set
+            {
+                col = (uint)((byte)value << 16) + (col & 0xff00ffff);
+            }
+        } 
+        public byte B
+        {
+            get => (byte)((col & 0x0000ff00) >> 8);
+            set
+            {
+                col = (uint)((byte)value << 8) + (col & 0xffff00ff);
+            }
+        } 
+        public byte A
+        {
+            get => (byte)((col & 0x000000ff) >> 0);
+            set
+            {
+                col = (uint)((byte)value) + (col & 0xffffff00);
+            }
+        }
+        public float Hue => HueFromRGB(R, G, B);
+        bool hsv = false;
+        public Color(byte r, byte g, byte b, byte a)
+        {
+            hsv = false;
+            col = (uint)(r << 24) + (uint)(g << 16) + (uint)(b << 8);// + (uint)(a);
         }
 
-        public Color(uint hex, bool hsv=false)
+        public Color(float h, float s, float v, byte a, bool _ = true)
         {
-            if (hsv)
-            {
-                // do hsv conversion
-            }
+            hsv = true;
+            col = HSVToRGB(h, s, v, a);
+        }
+
+        public Color(uint hex)
+        {
+            hsv = false;
             col = hex;
+        }
+
+        public bool HSV
+        {
+            get => hsv;
         }
 
         public Color Copy()
@@ -36,30 +75,98 @@ namespace Magician
             return new Color(R, G, B, A);
         }
 
-        public static int RGBToHSV(byte r, byte g, byte b)
+        public static uint HSVToRGB(float h, float s, float v, byte a)
         {
-            throw new NotImplementedException();
+            byte r = 0;
+            byte g = 0;
+            byte b = 0;
+            float phase = h % 360;
+            
+            if (phase < 60)
+            {
+                r = 255;
+                g = (byte)(phase / 60 * 255);
+                b = 0;
+            }
+            else if (phase < 120)
+            {
+                r = (byte)(255 - (phase-60) / 60 * 255);
+                g = 255;
+                b = 0;
+            }
+            else if (phase < 180)
+            {
+                r = 0;
+                g = 255;
+                b = (byte)((phase-120) / 60 * 255);
+            }
+            else if (phase < 240)
+            {
+                r = 0;
+                g = (byte)(255 - (phase-180)/ 60 * 255);
+                b = 255;
+            }
+            else if (phase < 300)
+            {
+                r = (byte)((phase-240) / 60 * 255);
+                g = 0;
+                b = 255;
+            }
+            else if (phase < 360)
+            {
+                r = 255;
+                g = 0;
+                b = (byte)(255 - (phase-300) / 60 * 255);
+            }
+            else
+            {
+                throw new InvalidDataException($"Invalid phase {phase}");
+            }
+            return (uint)(r << 24) + (uint)(g << 16) + (uint)(b << 8) + (uint)(a);
         }
 
-        public static int RGBToHSV(int hex)
+        private static float HueFromRGB(byte r, byte g, byte b)
         {
-            byte r = (byte)(hex & 0xff000000);
-            byte g = (byte)(hex & 0x00ff0000);
-            byte b = (byte)(hex & 0x0000ff00);
-            return RGBToHSV(r, g, b);
-        }
-
-        public static int HSVToRGB(byte h, byte s, byte v)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static int HSVToRGB(int hex)
-        {
-            byte h = (byte)(hex & 0xff000000);
-            byte s = (byte)(hex & 0x00ff0000);
-            byte v = (byte)(hex & 0x000000ff);
-            return HSVToRGB(h, s, v);
+            // Phase is from 0 to 60
+            if (r == 255 && b == 0)
+            {
+                return 60 * (float)g/255f;
+            }
+            // Phase is from 60 to 120
+            else if (g == 255 && b == 0)
+            {
+                return 60 + 60 * (255 - (float)r/255f);
+            }
+            // Phase is from 120 to 180
+            else if (g == 255 && r == 0)
+            {
+                return 120 + 60 * (float)b/255f;
+            }
+            // Phase is from 180 to 240
+            else if (b == 255 && r == 0)
+            {
+                return 180 + 60 * (255 - (float)g/255f);
+            }
+            // Phase is from 240 to 300
+            else if (b == 255 && g == 0)
+            {
+                return 240 + 60 * (float)r/255f;
+            }
+            // Phase is from 300 to 360
+            else if (r == 255 && g == 0)
+            {
+                return 300 + 60*(255 - (float)b/255f);
+            }
+            // Greyscale hue
+            else if (r + g + b == 0)
+            {
+                Console.WriteLine("Warning: attempted to get hue from greyscale");
+                return 0;
+            }
+            else
+            {
+                throw new InvalidDataException("Could not get Hue from RGB!");
+            }
         }
 
         public static Color RED
