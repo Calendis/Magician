5906933ca4a5ca3239a2ac566d600c9624c388a8
Calendis <calendis1@gmail.com>
2022-10-11 01:45:33 -0600

    Plot demo and refactor into list declaration

--- Demo.cs
+++ Demo.cs
@@ -8,9 +8,10 @@ namespace Magician
         static IntPtr renderer;
 
         bool done = false;
-        List<Multi> multis = new List<Multi>();
+        List<Drawable> mathObjs = new List<Drawable>();
         Random r = new Random();
         int frames = 0;
+        int driveDelay = 0;
         double timeResolution = 0.1;
         
         static void Main(string[] args)
@@ -33,26 +34,25 @@ namespace Magician
         void GameLoop()
         {
             /*
-                Game setup
+                What do we want to make?
             */
-            Multi m =
-            
-            Multi.RegularPolygon(0, 0, Color.Red.ToHSL(), 6, 90)
-            .SubDriven(
-                new Driver(x => 0.01), "phase+")
-            .Driven(
-                new Driver(x => 180 + 180*Math.Cos(x[0]*0.01/timeResolution)), "col0")
-            .Surrounding(
-                Multi.RegularPolygon(0, 0, 6, 90)
-                .Invisible())
-            .SubDriven(new Driver(x => 180*Math.Cos(x[0]*0.01/timeResolution)), "magnitude")
-            .Recursed();
-            ;
+            Quantity q = new Quantity(40);
+            mathObjs = new List<Drawable>()
+            {
+                
+                new Plot(-100, 100, new Driver(x => q.Driven(x =>
+                {
+                    return 100*Math.Abs(Math.Sin(x[0]/10));
+                }).Evaluate()*Math.Sin(x[0]/10)), -200, 200, 1, Color.Blue.ToHSL())
+                .Driven(x => 100-x[0], "dx")
+                .Driven(x => -0.17, "y+")
+                .Driven(x => 0.01 + Math.Sin(x[0]/10), "x+")
+                .Driven(x => x[0], "col0"),
+            };
 
-            //Plot p = new Plot(0, 0, new Driver(x => x[0]*x[0]/50), -50, 50, 0.1, Color.RED);            
-            //multis.Add(p.Interpolation().SubDriven(new Driver(x => 0.02), "phase+").Surrounding(Multi.RegularPolygon(0, 0, 5, 50).SubDriven(new Driver(x => 0.01), "phase+")));
+
+            //multis.Add(m);
             
-            multis.Add(m);
             /*
                 Main gameloop
             */
@@ -61,7 +61,10 @@ namespace Magician
                 //SDL_WaitEvent(out SDL_Event events);
                 SDL_PollEvent(out SDL_Event sdlEvent);
                 Render();
-                Drive();
+                if (frames > driveDelay)
+                {
+                    Drive();
+                }
 
                 // Event handling
                 switch (sdlEvent.type)
@@ -75,28 +78,56 @@ namespace Magician
         
         void Render()
         {
-            // Clear with colour
+            // Clear with colour)
             SDL_SetRenderDrawColor(renderer,
             Globals.bgCol.R, Globals.bgCol.G, Globals.bgCol.B, 255);
             SDL_RenderClear(renderer);
 
             // Draw objects
-            foreach(Multi m in multis)
+            foreach(Drawable d in mathObjs)
             {
-                m.Draw(ref renderer);
+                d.Draw(ref renderer);
             }
+
+            // SAVE FRAME TO IMAGE
+            IntPtr texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, 0, Globals.winWidth, Globals.winHeight);
+            IntPtr target = SDL_GetRenderTarget(renderer);
+    
+            int width, height;
+            SDL_SetRenderTarget(renderer, texture);
+            SDL_QueryTexture(texture, out _, out _, out width, out height);
+            IntPtr surface = SDL_CreateRGBSurfaceWithFormat(SDL_RLEACCEL, width, height, 0, SDL_PIXELFORMAT_ARGB8888);
+            SDL_Rect r = new SDL_Rect();
+            r.x = 0;
+            r.y = 0;
+            r.w = Globals.winWidth;
+            r.h = Globals.winHeight;
+            unsafe
+            {
+                SDL_Surface* surf = (SDL_Surface*)surface;
+                SDL_RenderReadPixels(renderer, ref r, SDL_PIXELFORMAT_ARGB8888, surf->pixels, surf->pitch);
+                SDL_SaveBMP(surface, $"saved/{frames}.png");
+                SDL_FreeSurface(surface);
+            }
+
 
             frames++;
 
             // Display
             SDL_RenderPresent(renderer);
+            //SDL_Delay(1/6);
         }
 
         void Drive()
         {
-            foreach (Multi m in multis)
+            foreach (Driveable d in mathObjs)
             {
-                m.Drive(frames * timeResolution);
+                d.Drive((frames-driveDelay) * timeResolution);
+            }
+
+            for (int i = 0; i < Quantity.ExtantQuantites.Count; i++)
+            {
+                Quantity.ExtantQuantites[i].Drive((frames-driveDelay) * timeResolution);
             }
         }
 
