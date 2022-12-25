62e5cc6ab6fb48e524dfc091c122ed93a918666f
Calendis <calendis1@gmail.com>
2022-12-24 18:27:30 -0700

    some operations for IMaps

--- magician/IMap.cs
+++ magician/IMap.cs
@@ -2,9 +2,32 @@ namespace Magician
 {
     public interface IMap
     {
-        // I'm surprised this is allowed
         public abstract double[] Evaluate(params double[] x);
         public abstract double Evaluate(double x);
+
+        // IMap operators
+        public virtual IMap Add(IMap o)
+        {
+            throw new NotImplementedException($"Method Add not supported on {this.GetType().Name}");
+        }
+        public virtual IMap Mult(IMap o)
+        {
+            throw new NotImplementedException($"Method Mult not supported on {this.GetType().Name}");
+        }
+        public virtual IMap Derivative()
+        {
+            throw new NotImplementedException($"Method Derivative not supported on {this.GetType().Name}");
+        }
+        public virtual IMap Integral()
+        {
+            throw new NotImplementedException($"Method Integral not supported on {this.GetType().Name}");
+        }
+
+        public virtual IMap Concat()
+        {
+            throw new NotImplementedException($"Method Concat not supported on {this.GetType().Name}");
+        }
+
         public Multi MultisAlong(double lb, double ub, double dx, Multi tmp, double xOffset=0, double yOffset=0, Func<double, double>? truth=null, double threshold=0)
         {
             if (truth is null)
@@ -31,10 +54,5 @@ namespace Magician
             m.parent = Multi.Origin;
             return m.DrawFlags(DrawMode.INVISIBLE);
         }
-    }
-
-    public interface IMapParallel
-    {
-        public abstract double[] EvaluateParallel(params double[] x);
     }
 }
