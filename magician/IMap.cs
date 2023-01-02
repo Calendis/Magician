73b9d080f41defae6f4591de013f4d8bc90f08cb
Calendis <calendis1@gmail.com>
2023-01-02 16:17:48 -0700

    Refactor plotting into IMap

--- magician/IMap.cs
+++ magician/IMap.cs
@@ -28,6 +28,7 @@ namespace Magician
             throw new NotImplementedException($"Method Concat not supported on {this.GetType().Name}");
         }
 
+        // Place Multis along an IMap according to some truth function
         public Multi MultisAlong(double lb, double ub, double dx, Multi tmp, double xOffset=0, double yOffset=0, Func<double, double>? truth=null, double threshold=0)
         {
             if (truth is null)
@@ -53,6 +54,42 @@ namespace Magician
             }
             m.parent = Multi.Origin;
             return m.DrawFlags(DrawMode.INVISIBLE);
+        }
+
+        // Render an IMap to a Multi
+        public Multi Plot(double x, double y, double start, double end, double dx, Color c)
+        {
+            List<Multi> points = new List<Multi>();
+            for (double t = start; t < end; t+=dx)
+            {
+                Multi[] ps = interpolate(t, t+dx);
+                ps[0].Col = c;
+                ps[1].Col = c;
+                points.Add(ps[0]);
+
+            }
+            Multi m = new Multi(x, y, c, DrawMode.PLOT, points.ToArray());
+            return m;
+        }
+
+        private Multi[] interpolate(double t0, double t1)
+        {            
+            double[] p0 = Evaluate(new double[] {t0});
+            double[] p1 = Evaluate(new double[] {t1});
+            
+            if (p0.Count() < 2)
+            {
+                p0 = new double[]{t0, p0[0]};
+            }
+            if (p1.Count() < 2)
+            {
+                p1 = new double[]{t1, p1[0]};
+            }
+
+
+            Multi mp0 = Multi.Point(p0[0], p0[1]);
+            Multi mp1 = Multi.Point(p1[0], p1[1]);
+            return new Multi[] {mp0, mp1};
         }
     }
 }
