2b6c99d7709033fefab31318b1fd2a2327002d5f
Calendis <calendis1@gmail.com>
2023-01-16 15:06:24 -0700

    text demo

--- spell/Spell.cs
+++ spell/Spell.cs
@@ -1,3 +1,6 @@
+// This class is where you create your stuff
+// It's static for now
+
 namespace Magician
 {
     static class Spell
@@ -20,18 +23,19 @@ namespace Magician
             Multi lissajous = ((IMap)new Driver(
                 t => new double[]{300*Math.Cos(t[0]/5), 300*Math.Sin(t[0])
                 }))
-            .Plot(0, 0, 0, 100, 1, new RGBA(0xff0000ff));
+            .Plot(0, 0, 0, 100, 0.1, new RGBA(0xff0000ff));
             Geo.Origin.Add(lissajous);
 
             // Load a texture
-            t = new Renderer.Texture("test.png", 400, 300);
+            //t = new Renderer.Texture("test.png", 400, 300);
+            t = new Renderer.Text("hello world :)", Ref.fgCol).Render();
         }
 
         // For stuff that needs to redefined every frame
         public static void Loop(ref int frames, ref double timeResolution)
         {
             Renderer.Control.Clear();
-            t.Draw();
+            t.Draw(500, 100);
         }
     }
 }
