892b39eb30a0a7cc658508451e377b884d3eb428
Calendis <calendis1@gmail.com>
2023-01-16 15:05:46 -0700

    Add font size global

--- magician/Ref.cs
+++ magician/Ref.cs
@@ -8,6 +8,7 @@ namespace Magician
     {
         public static int winWidth = 1200;
         public static int winHeight = 800;
+        public static int fontSize = 24;
 
         public static HSLA bgCol = new RGBA(0x001010ff).ToHSLA();
         public static HSLA fgCol = new RGBA(0x00e9f5ff).ToHSLA();
