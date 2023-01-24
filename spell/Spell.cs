551267bb6c256b5f57cf2538fbbb244a26f829e4
Calendis <calendis1@gmail.com>
2023-01-24 16:02:30 -0700

    Text/texture, Grid, Multi driving demo

--- spell/Spell.cs
+++ spell/Spell.cs
@@ -1,64 +1,45 @@
 // This class is where you create your stuff
 // It's static for now
 
+using Magician.Renderer;
+
 namespace Magician
 {
     static class Spell
     {
         // For stuff that can be defined once and left alone
         static Random rng = new Random();
-        static bool sw = true;
+        static Texture? tx0;
+        static Texture? tx1;
 
-        static double theta = 0.00;  // This controls the broad shape of the figure, from 0 to 2PI
-        static double scale;
-        static double modulus = rng.NextDouble();
-        
-        static int iters = 150;
-        static int noPoints = 24000;
+        static Multi vertex;
+
+
         public static void PreLoop(ref int frames, ref double timeResolution)
         {
-            //Renderer.Control.Clear();
-            for (int i = 0; i < noPoints; i++)
-            {
-                Geo.Origin.Add(
-                    Geo.Point(Ref.RandX, Ref.RandY).Colored(new RGBA(0x00ffff10))
-                    .Colored(new HSLA(0, 1, 1, 6))
-                );
-                scale = 4d/modulus;
-            }
+            Renderer.Control.Clear();
+            Geo.Origin.Add(UI.Presets.Graph.Cartesian());
+
+            Geo.Origin.Add(
+                Geo.RegularPolygon(0, 0, new RGBA(0x00f010a0), 5, 120)
+                .Sub(m => m.Driven(
+                    x => 0.01, "phase+"
+                    )
+                    .Textured(new Text("Maybe??", new RGBA(0x0000ffff)).Render())
+                )
+            );
+
+            IMap i = new Seq(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,39,39);
+            Geo.Origin.Add(i.Plot(0, -200, -10, 50, 0.1, new RGBA(0xffff00ff)));
+
+            Geo.Origin.csts[1].csts[0].Textured(new Renderer.Text("Yes!", new RGBA(0xff00ffff)).Render());
+
         }
 
         // For stuff that needs to redefined every frame
         public static void Loop(ref int frames, ref double timeResolution)
         {
-            //Renderer.Control.Clear();
-            double t = frames * timeResolution;
-            Geo.Origin.Sub(
-                m =>
-                {
-                    m.Written(m.X.Evaluate())
-                    .Rotated(Math.PI / 2)
-                    .Translated((t + m.Normal) % (modulus) * 100 * scale, 0)
-                    .Scaled(Math.Sin(theta + m.Index))
-                    .Colored(new HSLA((m.Evaluate() - m.X.Evaluate()) / 80, 1, 1, 8));
-                }
-            );
-
-            if (frames % iters == 0 && frames != 0)
-            {
-                Renderer.Control.Clear(new RGBA(0x000000ff));
-                Geo.Origin.Sub(m =>
-                    m.Translated(-m.X.Evaluate(), -m.Y.Evaluate()).Translated(Ref.RandX, Ref.RandY)
-                );
-                modulus = rng.NextDouble();
-                scale = 2d/modulus;
-                //Renderer.Control.saveFrame = true;
-            }
-            else
-            {
-                Renderer.Control.saveFrame = false;
-            }
-            
+            Renderer.Control.Clear();
         }
     }
 }
