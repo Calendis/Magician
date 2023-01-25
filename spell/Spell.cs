5e139fcb7901d7ff40e0baf73746f065a3aff8a5
Calendis <calendis1@gmail.com>
2023-01-25 13:30:25 -0700

    IMap driven demo

--- spell/Spell.cs
+++ spell/Spell.cs
@@ -20,19 +20,15 @@ namespace Magician
             Renderer.Control.Clear();
             Geo.Origin.Add(UI.Presets.Graph.Cartesian());
 
+            Multi pentagon = Geo.RegularPolygon(0, 0, new RGBA(0x00f01070), 5, 120)
+                .Sub(m => m
+                    .Textured(new Text($"{m.Index}", new RGBA(0xff00ffff)).Render())
+                );
+
             Geo.Origin.Add(
-                Geo.RegularPolygon(0, 0, new RGBA(0x00f010a0), 5, 120)
-                .Sub(m => m.Driven(
-                    x => 0.01, "phase+"
-                    )
-                    .Textured(new Text("Maybe??", new RGBA(0x0000ffff)).Render())
-                )
+                pentagon
             );
-
-            IMap i = new Seq(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,39,39);
-            Geo.Origin.Add(i.Plot(0, -200, -10, 50, 0.1, new RGBA(0xffff00ff)));
-
-            Geo.Origin.csts[1].csts[0].Textured(new Renderer.Text("Yes!", new RGBA(0xff00ffff)).Render());
+            pentagon.Driven(new Seq(1,2,3,4,5,6,7,8,9,10), "x+");
 
         }
 
