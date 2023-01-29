aeb5040b242ba6058660d825bf5f72494523f964
Calendis <calendis1@gmail.com>
2023-01-28 17:20:57 -0700

    TextAlong demo

--- spell/Spell.cs
+++ spell/Spell.cs
@@ -16,27 +16,24 @@ namespace Magician
         // For stuff that can be defined once and left alone
         static Random rng = new Random();
         static UI.Grid uiGrid;
-        static Multi pentagon;
-        static Multi bigText;
+
+        // Initializations
+        static Spell()
+        {
+            uiGrid = UI.Presets.Graph.Cartesian();
+        }
 
         public static void PreLoop(ref int frames, ref double timeResolution)
         {
             Renderer.Control.Clear();
-            uiGrid = UI.Presets.Graph.Cartesian();
             Origin.Add(uiGrid.Render());
+            Origin.Add(new Multi());
 
-            //
 
-            Perspective.x.Driven(x => 50*Math.Sin(x[0]/10));
-            Perspective.y.Driven(x => 50*Math.Cos(x[0]/10));
+            Perspective.x.Driven(x => 50 * Math.Sin(x[0] / 10));
+            Perspective.y.Driven(x => 50 * Math.Cos(x[0] / 10));
             //Quantity.ExtantQuantites.Add(Perspective.x);
             //Quantity.ExtantQuantites.Add(Perspective.y);
-            bigText = ((IMap)new Driver(x => 64*Math.Sin(x[0]/40)))
-            .MultisAlong(-100, 100, 10*Math.PI, new Multi().Textured(new Text("a", new RGBA(0x00ffffff)).Render()));
-            Origin.Add(bigText);
-
-            Origin.Add(((IMap)new Driver(x => 64*Math.Sin(x[0]/40)))
-            .TextAlong(-200, 200, 10*Math.PI, "hellotheremyfriendsletshavesomefunyahooooooooooooooooooooooooooo"));
 
         }
 
@@ -46,6 +43,11 @@ namespace Magician
             Renderer.Control.Clear();
             uiGrid = uiGrid.Update();
             Origin.csts[0] = uiGrid.Render();
+
+            double t = frames * timeResolution;
+
+            Origin.csts[1] = (((IMap)new Driver(x => 16 * Math.Sin(x[0] / 40 + t)))
+                .TextAlong(-200, 600, 10 * Math.PI, "Hello there, my friends", new HSLA(t, 1, 1, 255)));
             //Console.WriteLine(Perspective.x);
         }
     }
