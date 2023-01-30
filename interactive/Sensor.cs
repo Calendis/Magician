439988c1908fe4b5e545d84c388d7ee3671dc168
Calendis <calendis1@gmail.com>
2023-01-30 14:41:29 -0700

    Refactor MouseOver, add wheel support

--- interactive/Sensor.cs
+++ interactive/Sensor.cs
@@ -7,49 +7,61 @@ namespace Magician.Interactive
         public abstract double[] Evaluate(params double[] x);
 
         public abstract double Evaluate(double x);
+
+        public static IMap MouseOver(Multi m)
+        {
+            return new DirectMap(b => Geo.Check.PointInPolygon(Events.MouseX, Events.MouseY, m) ? 1 : 0);
+        }
     }
 
-    /*
-    public class ASCII : Sensor, IMap
-    {
-        //public double
-    }
-    */
-
-    public class MouseOver : Sensor, IMap
+    public class ScrollOver : Sensor, IMap
     {
         Multi m;
-        public MouseOver(Multi m)
+        public ScrollOver(Multi m)
         {
             this.m = m;
         }
 
         public override double Evaluate(double x)
         {
-            return Geo.Check.PointInPolygon(Events.MouseX, Events.MouseY, m)?1:0;
+            return Geo.Check.PointInPolygon(Events.MouseX, Events.MouseY, m) ? Events.ScrollY : 0;
         }
 
         public override double[] Evaluate(params double[] x)
         {
-            return new double[] {Geo.Check.PointInPolygon(Events.MouseX, Events.MouseY, m)?1:0};
+            return Geo.Check.PointInPolygon(Events.MouseX, Events.MouseY, m) ? new double[] {Events.ScrollX, Events.ScrollY} : new double[] {0, 0};
         }
     }
 
     public static class Events
     {
+        // Keymap generated from SDL enum
         public static Dictionary<SDL_Keycode, bool> keys = new Dictionary<SDL_Keycode, bool>();
-        
-        static int[] mouse = new int[2];
+
+        // Flags
         static bool getMouse = false;
+        static bool getScroll = false;
+
+        // Event state
+        static int[] mouse = new int[2];
+        static float[] scroll = new float[2];
 
         public static double MouseX
         {
-            get => (double)mouse[0] - Globals.winWidth/2;
+            get => (double)mouse[0] - Globals.winWidth / 2;
         }
-
         public static double MouseY
         {
-            get => (double)-mouse[1] + Globals.winHeight/2;
+            get => (double)-mouse[1] + Globals.winHeight / 2;
+        }
+
+        public static double ScrollX
+        {
+            get => (double)scroll[0];
+        }
+        public static double ScrollY
+        {
+            get => (double)scroll[1];
         }
 
         static Events()
@@ -67,17 +79,19 @@ namespace Magician.Interactive
             {
                 case SDL_EventType.SDL_KEYDOWN:
                     keys[e.key.keysym.sym] = true;
-                    //goto case SDL_EventType.SDL_KEYUP;
                     break;
-                
+
                 case SDL_EventType.SDL_KEYUP:
                     keys[e.key.keysym.sym] = false;
-                    //goto case SDL_EventType.SDL_MOUSEMOTION;
                     break;
 
                 case SDL_EventType.SDL_MOUSEMOTION:
                     getMouse = true;
-                    //SDL_GetMouseState(out mouse[0], out mouse[1]);
+                    break;
+
+                case SDL_EventType.SDL_MOUSEWHEEL:
+                    scroll[0] = e.wheel.preciseX;
+                    scroll[1] = e.wheel.preciseY;
                     break;
 
                 default:
@@ -96,6 +110,7 @@ namespace Magician.Interactive
                 getMouse = false;
                 SDL_GetMouseState(out mouse[0], out mouse[1]);
             }
+
         }
     }
 }
