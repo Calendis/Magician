27fcf6fe5af36e79e88c53a348a3a0529c0fff5a
Calendis <calendis1@gmail.com>
2023-02-06 13:16:18 -0700

    Remove Presets

--- magician/ui/Presets.cs
+++ magician/ui/Presets.cs
@@ -1,15 +1,4 @@
 namespace Magician.UI.Presets
 {
-    public static class Graph
-    {
-        public static UI.Grid Cartesian(double spacing = 100)
-        {
-            return new UI.Grid(spacing, Math.Sqrt(spacing), spacing, Math.Sqrt(spacing));
-        }
-    }
-
-    public static class State
-    {
-        //
-    }
+    // TODO: add some preset
 }
