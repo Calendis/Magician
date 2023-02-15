654d707d24401c67d0e2c90b82156051b0cae5a2
Calendis <calendis1@gmail.com>
2023-02-15 00:44:18 -0700

    Buttons! :)

--- magician/interactive/Controls.cs
+++ magician/interactive/Controls.cs
@@ -7,8 +7,36 @@ namespace Magician.Interactive
         //
     }
 
-    public class Button : Multi, IControl
+    public class Button : Multi
     {
-        //
+        IMap mo;
+        bool hovered = false;
+        Color hoverCol = HSLA.RandomVisible();
+        Color tempCol;
+        public Button(double x, double y, double width, double height) : base(x, y)
+        {
+            Become(Geo.Create.Rect(x, y, width, height));
+            mo = Sensor.MouseOver(this);
+            tempCol = col;
+        }
+
+        public override void Update()
+        {
+            hovered = mo.Evaluate() > 0;
+
+            if (hovered)
+            {
+                col = hoverCol;
+            }
+            else
+            {
+                col = tempCol;
+            }
+
+            if (hovered && Sensor.Click.Evaluate()>0)
+            {
+                col = HSLA.RandomVisible();
+            }
+        }
     }
 }
--- magician/interactive/Sensors.cs
+++ magician/interactive/Sensors.cs
@@ -2,6 +2,7 @@ namespace Magician.Interactive
 {
     public abstract class Sensor : CustomMap
     {
+        public static IMap Click = new CustomMap(b => Events.Click ? 1 : 0);
         public static IMap MouseOver(Multi m)
         {
             return new CustomMap(b => Geo.Check.PointInPolygon(Events.MouseX, Events.MouseY, m) ? 1 : 0);
