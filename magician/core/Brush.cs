b8e8252eb5b74a15603763c9b850cae7165c29b0
Calendis <calendis1@gmail.com>
2023-02-24 18:06:00 -0700

    Brush

---
+++ magician/core/Brush.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Magician
+{
+    public class Brush : IOMap
+    {
+        public Brush(IMap im0, IMap im1) : base(1, im0, im1)
+        {
+            //
+        }
+
+        public Multi Paint(double t, Multi m)
+        {
+            double[] pos = base.Evaluate(new double[]{t});
+            if (t > 0)
+            {
+                return m.Positioned(pos[0], pos[1]).Pasted();
+            }
+            return new Multi().Tagged("empty paint");
+        }
+
+        public void PaintPolygon(double t, int sides, double mag, Color? c=null)
+        {
+            c ??= HSLA.RandomVisible();
+            Paint(t, Geo.Create.RegularPolygon(sides, mag).Colored(c));
+        }
+    }
+}
