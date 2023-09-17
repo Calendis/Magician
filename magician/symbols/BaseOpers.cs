edaa3ec1154a013bd757e82f851e43058d3f42b7
Calendis <calendis1@gmail.com>
2023-09-16 22:24:54 -0600

    Fixed regression in Degree

--- magician/symbols/BaseOpers.cs
+++ magician/symbols/BaseOpers.cs
@@ -36,50 +36,17 @@ public class SumDiff : Oper
     {
         double minD = double.MaxValue;
         double maxD = double.MinValue;
-        foreach (Oper o in _oldArgs)
+        foreach (Oper o in AllArgs)
         {
             double d = o.Degree(v);
             minD = d < minD ? d : minD;
             maxD = d > maxD ? d : maxD;
         }
-        return Math.Abs(minD) + maxD;
-    }
-
-    public override void Simplify()
-    {
-        //ArgsOLD = Associate<SumDiff>();
-
-        // Combine constants
-        List<(Oper, bool)> flaggedArgs = new();
-        double total = 0;
-        int switcher = 0;
-        foreach (Oper o in _oldArgs)
-        {
-            if (o is Variable v && v.Found)
-            {
-                total += v.Val * switcher == 0 ? 1 : -1;  // TODO: use this total
-            }
-            else
-            {
-                flaggedArgs.Add((o, switcher == 1));
-            }
-
-            switcher = 1 - switcher;
-        }
-        //args = AssembleArgs(flaggedArgs, identity);
-        //numArgs = args.Length;
-
-        // TODO: Combine like variables
-        //
-
-        // Modify
-        //ArgsOLD = AssembleArgs(flaggedArgs, identity);
+        return maxD - minD;
     }
 
     public override string ToString()
     {
-        //string argsSigns = $"{string.Join("", _oldArgs.Select((a, i) => i % 2 == 0 ? $"+{a.ToString()}" : $"-{a.ToString()}"))}";
-        //return "(" + string.Join("", argsSigns.Skip(1)) + ")";
         string sumdiff = "";
         if (posArgs.Count > 0)
         {
@@ -93,7 +60,7 @@ public class SumDiff : Oper
         {
             sumdiff += "-" + o.ToString();
         }
-        return sumdiff + ")";
+        return "(" + sumdiff + ")";
     }
 }
 
@@ -130,15 +97,10 @@ public class Fraction : Oper
         return new Fraction(a, b);
     }
 
+    // TODO: when you introduce power laws, Degree is going to need to be an Oper
     public override double Degree(Variable v)
     {
-        return _oldArgs.Select<Oper, double>((o, i) =>
-        {
-            if (i % 2 == 0)
-                return o.Degree(v);
-            else
-                return -o.Degree(v);
-        }).Sum();
+        return posArgs.Select(o => o.Degree(v)).Sum() - negArgs.Select(o => o.Degree(v)).Sum();
     }
 
     public override string ToString()
