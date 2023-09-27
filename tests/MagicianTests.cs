bb8f5adfc7be68cb611b6f79d1563f99a8065497
Calendis <calendis1@gmail.com>
2023-09-26 18:37:25 -0600

    Writing tests for algebra

--- tests/MagicianTests.cs
+++ tests/MagicianTests.cs
@@ -1,7 +1,8 @@
-
 namespace Magician.Tests;
+using Magician.Symbols;
+using static Magician.Symbols.Notate;
+using NUnit.Framework;
 
-/*
 public class Tests
 {
     [SetUp]
@@ -10,30 +11,53 @@ public class Tests
     }
 
     [Test]
-    public void AddOneAndOne()
-    {
-        Oper onePlusOne = new Plus(N(1), N(1));
-        Variable result = (Variable)onePlusOne.Eval();
-        Assert.That(result.Val, Is.EqualTo(2));
-    }
-
-
-    [Test]
     public void MultiplyTwoAndTen()
     {
-        Oper twoTimesTen = new Mult(N(2), N(10));
-        Variable result = twoTimesTen.Eval();
+        Oper twoTimesTen = new Fraction(Val(2), Val(1), Val(10));
+        Variable result = twoTimesTen.Solution();
         Assert.That(result.Val, Is.EqualTo(20));
     }
 
     [Test]
-    public void SolveEquationWithSum()
+    public void AssociateSumDiffs()
     {
-        Equation equation = new Equation(new Plus(Let("x"), N(2)), Equation.Fulcrum.EQUALS, N(10));
-        Assert.That(equation.ToString(), Is.EqualTo(@"Plus(Variable(x`ariable(2)) = Variable(10)"));
+        SumDiff sd = new(
+            new List<Oper>{new SumDiff(Val(2)), new SumDiff(Val(1), Val(2), new SumDiff(Val(3)), Val(4)), Val(1), new SumDiff(Val(3))},
+            new List<Oper>{Val(5), new SumDiff(Val(1), new SumDiff(Val(5), new SumDiff(Val(3))))}
+        );
+        Assert.That(sd.Solution().Val, Is.EqualTo(0));
+        sd.Associate();
+        Assert.That(sd.Solution().Val, Is.EqualTo(0));
+    }
 
-        Equation solved = equation.Solve(Let("x"));
-        Assert.That(solved.ToString(), Is.EqualTo(@"Variable(x) = Minus(Variable(10), Variable(2))"));
+    [Test]
+    public void SolveAddLike()
+    {
+        Equation unsolved = new(
+            new SumDiff(
+                new Fraction(Var("x"), Val(1), Var("y")),
+                new Fraction(Var("x"), Val(1), Var("y"), Var("x")),
+                new Fraction(Var("y"), Var("z")),
+                new Fraction(Var("y"), Val(1), Var("z"), Val(1), Var("z"))
+            ),
+            Fulcrum.EQUALS,
+            Val(1)
+        );
+        SolvedEquation solved = unsolved.Solved();
+        SolvedEquation manuallySolved = new(
+            Var("y"),
+            Fulcrum.EQUALS,
+            new Fraction(
+                Val(1),
+                new SumDiff(
+                    Var("x"),
+                    new Fraction(Var("x"), Val(1), Var("x")),
+                    Var("z"),
+                    new Fraction(Var("z"), Val(1), Var("z"))
+                )
+            ),
+            Var("y")
+        );
+        Assert.That(solved.Evaluate(10.5, -3), Is.EqualTo(manuallySolved.Evaluate(10.5, -3)));
     }
 }
-*/
