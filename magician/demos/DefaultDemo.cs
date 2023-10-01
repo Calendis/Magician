5ac0b7635742a513e4afb927682296e71c46a7f8
Calendis <calendis1@gmail.com>
2023-09-30 23:33:31 -0600

    More algebra testing

--- magician/demos/DefaultDemo.cs
+++ magician/demos/DefaultDemo.cs
@@ -37,7 +37,7 @@ public class DefaultSpell : Spell
         () =>
         {
             Scribe.Info("Switching Spells...");
-            Spellcaster.Prepare(new Demos.Tests.MapPlotting());
+            Spellcaster.Prepare(new Demos.Tests.EqPlotting());
             Spellcaster.Cast();
         }
 
