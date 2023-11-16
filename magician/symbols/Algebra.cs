430fdce03e31e48efbf3bf5b9bbe4a689b9a34b3
Calendis <calendis1@gmail.com>
2023-11-16 02:40:13 -0700

    greatly simplified add-like-terms code, remove add constant terms code

--- magician/symbols/Algebra.cs
+++ magician/symbols/Algebra.cs
@@ -51,111 +51,28 @@ public abstract partial class Oper
             chosenSide.negArgs.Remove(axis);
         else
             throw Scribe.Issue($"Could not extract {axis} from {chosenSide.GetType()} {chosenSide}");
-        return (chosenSide.Trim(), chosenSide.New(new List<Oper>{oppositeSide}, new List<Oper>{axis}).Trim());
-    }
-
-    public static void CombineConstantTerms(SumDiff sd)
-    {
-        /* Combine constant integer terms */
-        IEnumerable<Oper> plus = sd.posArgs.Where(o => o is Variable v && v.Found && (int)v.Val == v.Val && (int)v.Val != sd.identity);
-        IEnumerable<Oper> minus = sd.negArgs.Where(o => o is Variable v && v.Found && (int)v.Val == v.Val && (int)v.Val != sd.identity);
-        List<Oper> posChaff = sd.posArgs.Where(o => !(o is Variable v && v.Found && (int)v.Val == v.Val && (int)v.Val != sd.identity)).ToList();
-        List<Oper> negChaff = sd.negArgs.Where(o => !(o is Variable v && v.Found && (int)v.Val == v.Val && (int)v.Val != sd.identity)).ToList();
-        int newPlus = plus.Aggregate(0, (total, o) => total + (int)((Variable)o).Val);
-        int newMinus = minus.Aggregate(0, (total, o) => total + (int)((Variable)o).Val);
-        int reducedConstant = newPlus - newMinus;
-        if (reducedConstant < 0)
-            negChaff.Add(new Variable(-reducedConstant));
-        else if (reducedConstant > 0)
-            posChaff.Add(new Variable(reducedConstant));
-        sd.posArgs.Clear(); sd.posArgs.AddRange(posChaff);
-        sd.negArgs.Clear(); sd.negArgs.AddRange(negChaff);
+        return (chosenSide.Trim(), chosenSide.New(new List<Oper> { oppositeSide }, new List<Oper> { axis }).Trim());
     }
 
     public static void CombineLikeTerms(SumDiff sd, Variable axis)
     {
-
-        //Scribe.Warn($"  Combining like terms for {sd} in terms of {axis}");
         List<Oper> finalPosArgs = new();
         List<Oper> finalNegArgs = new();
-        List<(Oper, bool)> termsContainingAxis = new();
-        List<(Oper, bool)> termsNotContainingAxis = new();
 
-        // Separate out terms containing our axis, and separate positive from negative
-        // This ensures our axis remains on the outside
-        foreach (Oper po in sd.posArgs)
-            if (po.Contains(axis))
-                termsContainingAxis.Add((po, true));
-            else if (po is not Variable v || !v.Found)
-                termsNotContainingAxis.Add((po, true));
-            else if (v.Found)
-                finalPosArgs.Add(v);
-        foreach (Oper no in sd.negArgs)
-            if (no.Contains(axis))
-                termsContainingAxis.Add((no, false));
-            else if (no is not Variable v || !v.Found)
-                termsNotContainingAxis.Add((no, false));
-            else if (v.Found)
-                finalNegArgs.Add(v);
-        //Scribe.Warn($"  TCA: {Scribe.Expand<IEnumerable<(Oper, bool)>, (Oper, bool)>(termsContainingAxis)}");
-        //Scribe.Warn($"  NCA: {Scribe.Expand<IEnumerable<(Oper, bool)>, (Oper, bool)>(termsNotContainingAxis)}");
+        var (fhs, gao) = sd.FlaggedHandshakes(axis);
+        List<List<(int, int, bool, bool)>> grpFlagHandshakes = fhs;
+        List<List<(Oper, bool)>> grpAxisOpers = gao;
 
-        foreach (List<(Oper, bool)> separatedTerms in new List<List<(Oper, bool)>> { termsContainingAxis, termsNotContainingAxis })
+        for (int c = 0; c < 2; c++)
         {
-            List<(int, int, bool, bool)> handshakes = new();
-            Dictionary<int, List<(int, int, bool, bool)>> termsToHandshakes = new();
-
-            // Trivial case
-            if (separatedTerms.Count == 1)
-            {
-                if (separatedTerms[0].Item2)
-                {
-                    finalPosArgs.Add(separatedTerms[0].Item1);
-                    continue;
-                }
-                else
-                {
-                    finalNegArgs.Add(separatedTerms[0].Item1);
-                    continue;
-                }
-            }
-
-            List<(Oper, bool)> termsToCombine = separatedTerms.Count % 2 == 1 ? separatedTerms.Append((new Variable(0), true)).ToList() : separatedTerms;
-            int counter = 0;
-            for (int i = 0; i < termsToCombine.Count; i++)
-            {
-                for (int j = i + 1; j < termsToCombine.Count; j++)
-                {
-                    Oper A = termsToCombine[i].Item1;
-                    Oper B = termsToCombine[j].Item1;
-                    //bool positive = !(termsToCombine[i].Item2 ^ termsToCombine[j].Item2);
-                    bool aPositive = termsToCombine[i].Item2;
-                    bool bPositive = termsToCombine[j].Item2;
-                    (int, int, bool, bool) flaggedHandshake = (i, j, aPositive, bPositive);
-                    handshakes.Add(flaggedHandshake);
-
-                    if (termsToHandshakes.ContainsKey(i))
-                        termsToHandshakes[i].Add(flaggedHandshake);
-                    else
-                        termsToHandshakes.Add(i, new List<(int, int, bool, bool)> { flaggedHandshake });
-                    if (termsToHandshakes.ContainsKey(j))
-                        termsToHandshakes[j].Add(flaggedHandshake);
-                    else
-                        termsToHandshakes.Add(j, new List<(int, int, bool, bool)> { flaggedHandshake });
-
-                }
-                counter++;
-            }
-            // Pick a handshake for each term
-            //List<Oper> termsNeedingHandshake = termsToCombine.Select(ob => ob.Item1).ToList();
+            List<(int, int, bool, bool)> flaggedHandshakes = grpFlagHandshakes[c];
+            List<(Oper, bool)> termsToCombine = grpAxisOpers[c];
             List<int> termsNeedingHandshake = Enumerable.Range(0, termsToCombine.Count).ToList();
+
             List<int> termsFoundHandshake = new();
             while (termsNeedingHandshake.Count > 0)
             {
-                //int termIdx = 0;//termsNeedingHandshake[0];
-                //Scribe.Warn($"Term {termsNeedingHandshake[termIdx]} needs a handshake, remaining: {termsNeedingHandshake.Count} ({Scribe.Expand<List<int>, int>(termsNeedingHandshake)})");
-                // Pick one of the possible handshakes for this term
-                foreach ((int, int, bool, bool) flaggedHandshake in termsToHandshakes[termsNeedingHandshake[0]])
+                foreach ((int, int, bool, bool) flaggedHandshake in flaggedHandshakes)
                 {
                     int i = flaggedHandshake.Item1;
                     int j = flaggedHandshake.Item2;
@@ -164,20 +81,18 @@ public abstract partial class Oper
                     bool aPositive = flaggedHandshake.Item3;
                     bool bPositive = flaggedHandshake.Item4;
                     bool positive = !(aPositive ^ bPositive);
+                    
                     if (termsFoundHandshake.Contains(i) || termsFoundHandshake.Contains(j))
-                    {
-                        //Scribe.Warn($"  Skipping missed handshake {i}, {j}...");
                         continue;
-                    }
 
                     Oper AB = Intersect(A, B);
-                    SumDiff ABbar;
+                    Oper ABbar;
                     if (positive)
                         ABbar = A.Divide(AB).Add(B.Divide(AB));
                     else if (aPositive)
                         ABbar = A.Divide(AB).Subtract(B.Divide(AB));
                     else if (bPositive)
-                        ABbar = A.Divide(AB).Subtract(B.Divide(AB));
+                        ABbar = B.Divide(AB).Subtract(A.Divide(AB));
                     else
                         throw Scribe.Issue("haggu");
 
@@ -204,13 +119,12 @@ public abstract partial class Oper
                     break;
                 }
             }
+            // Apply the changes
+            sd.posArgs.Clear();
+            sd.posArgs.AddRange(finalPosArgs);
+            sd.negArgs.Clear();
+            sd.negArgs.AddRange(finalNegArgs);
         }
-        // Apply the changes
-        //Scribe.Info($"  Got {new SumDiff(finalPosArgs, finalNegArgs)}");
-        sd.posArgs.Clear();
-        sd.posArgs.AddRange(finalPosArgs);
-        sd.negArgs.Clear();
-        sd.negArgs.AddRange(finalNegArgs);
     }
 
     public static Oper Intersect(Oper o, Oper p)
@@ -248,8 +162,7 @@ public abstract partial class Oper
         }
         foreach (Oper c in o.posArgs)
         {
-            int matches;
-            d.TryGetValue(c, out matches);
+            d.TryGetValue(c, out int matches);
             if (matches > 0)
             {
                 yield return c.Copy();
@@ -268,8 +181,7 @@ public abstract partial class Oper
         }
         foreach (Oper c in o.negArgs)
         {
-            int matches;
-            d.TryGetValue(c, out matches);
+            d.TryGetValue(c, out int matches);
             if (matches > 0)
             {
                 yield return c.Copy();
