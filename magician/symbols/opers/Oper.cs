6dc81be2ca8bf84a1c2b6a7432e769280a65ac75
Calendis <calendis1@gmail.com>
2023-11-26 06:18:02 -0700

    Clarify simplify/reduce methods, add Factors method

--- magician/symbols/opers/Oper.cs
+++ magician/symbols/opers/Oper.cs
@@ -8,36 +8,30 @@ public abstract partial class Oper : IFunction
     public List<Oper> posArgs = new();
     public List<Oper> negArgs = new();
     public List<Oper> AllArgs => posArgs.Concat(negArgs).ToList();
-    public readonly List<Variable> AssociatedVars = new();
+    public List<Variable> AssociatedVars = new();
 
     protected readonly string name;
     protected bool associative = false;
     protected bool commutative = false;
-    public bool unaryAssociative = true;
+    public bool trivialAssociative = true;
     public bool invertible = true;
 
     public int Ins { get; set; }
-    // TODO: move to form or something
     public bool IsConstant => this is Variable v && v.Found;
-    public bool IsDetermined {get {
-        return IsConstant || (AssociatedVars.Count == 0 && this is not Variable);
-        //try
-        //{
-        //    Solution();
-        //    return true;
-        //}
-        //catch (Scribe.MagicianError)
-        //{
-        //    return false;
-        //}
-    }}
+    public bool IsDetermined
+    {
+        get
+        {
+            return IsConstant || (AssociatedVars.Count == 0 && this is not Variable);
+        }
+    }
     public bool IsUnary => AllArgs.Count == 1 && posArgs.Count == 1;
-    public bool IsTrivial => unaryAssociative && IsUnary;
+    public bool IsTrivial => trivialAssociative && IsUnary;
 
     // Create a new Oper of the same type
     public abstract Oper New(IEnumerable<Oper> pa, IEnumerable<Oper> na);
     public abstract Variable Solution();
-    public abstract Oper Degree(Variable v);
+    public abstract Oper Degree(Oper v);
 
     protected Oper(string name, IEnumerable<Oper> posa, IEnumerable<Oper> nega)
     {
@@ -55,10 +49,10 @@ public abstract partial class Oper : IFunction
         OperLayers ol = new(this, Variable.Undefined);
         // TODO: obfuscate this behind some string constant
         // This is a hack, but it works. Normally I would check the found property of the Variable, but this is the
-        // base ctor, which is called before that property can be set. Another alternative might be to seperate
-        // variables and constants into distinct classes
+        // base ctor, which is called before that property can be set
         if (!(this is Variable && new string(name.Take(8).ToArray()) == "constant"))
             AssociatedVars = ol.GetInfo(0, 0).assocArgs.Distinct().ToList();
+        //AssociatedVars.Sort((v0, v1) => v0.Name[0] < v1.Name[0] ? 1 : v0.Name[0] > v1.Name[0] ? -1 : 0);
     }
     // Alternating form ctor. Handy in some cases
     protected Oper(string name, params Oper[] cstArgs) : this(name, cstArgs.Where((o, i) => i % 2 == 0).ToList(), cstArgs.Where((o, i) => i % 2 == 1).ToList()) { }
@@ -67,42 +61,59 @@ public abstract partial class Oper : IFunction
     protected virtual int? Identity { get; }
     // Deep-copies an Oper. Unknown variables always share an instance, however (see Variable.Copy)
     public virtual Oper Copy() { return New(posArgs.Select((o, i) => ((Oper)o).Copy()).ToList(), negArgs.Select((o, i) => ((Oper)o).Copy()).ToList()); }
-    
+
     // Perform basic simplifications on this Oper
     public abstract void ReduceOuter();
     // Perform basic simplifications on this Oper, recursively
-    public void Reduce()
+    /* TODO: eventually make the void simplification/reduction methods internal. */
+    /*       They modify state in a way that's safe inside the algebra solver,   */
+    /*       but not necessarily otherwise. Returning a new Oper is always safe. */
+    public void Reduce(int depth=-1)
     {
+        if (depth == 0)
+            return;
+        if (depth > 0)
+            depth --;
+        Associate();
         foreach (Oper a in AllArgs)
-            a.Reduce();
+            a.Reduce(depth);
         ReduceOuter();
     }
     // Perform advanced simplifications on this Oper
-    public virtual void SimplifyOuter(Variable? axis=null){}
-    // Perform advanced simplifications on this Oper, recursivelyz
-    public void SimplifyAll(Variable? axis=null)
+    internal virtual void SimplifyOnceOuter(Variable? axis = null) { ReduceOuter(); }
+    // Perform advanced simplifications on this Oper, recursivel
+    public void SimplifyOnce(Variable? axis = null, int depth=1)
     {
+        if (depth == 9)
+            return;
+        if (depth > 0)
+            depth--;
         foreach (Oper a in AllArgs)
-            a.SimplifyAll(axis);
-        SimplifyOuter(axis);
+            a.SimplifyOnce(axis, depth);
+        SimplifyOnceOuter(axis);
     }
     // Perform advanced simplifications in this Oper as much as possible
-    public void SimplifyFull(Variable? axis=null)
+    public void Simplify(Variable? axis = null, int bailout=9999)
     {
+        int iters = 0;
         Oper prev;
-        do 
+        do
         {
             prev = Copy();
-            SimplifyOuter(axis);
-            ReduceOuter();
+            SimplifyOnceOuter(axis);
+            if (++iters >= bailout)
+            {
+                Scribe.Warn("Bailed out after {iters} simplifications");
+                break;
+            }
         } while (!Like(prev));
     }
     // Perform advanced simplifications in this Oper as much as possible, recursively
-    public void SimplifyFullAll(Variable? axis=null)
+    public void SimplifyAll(Variable? axis = null)
     {
         foreach (Oper a in AllArgs)
-            a.SimplifyFullAll(axis);
-        SimplifyFull(axis);
+            a.SimplifyAll(axis);
+        Simplify(axis);
     }
 
     // Provide arguments to solve the expression at a point
@@ -136,6 +147,12 @@ public abstract partial class Oper : IFunction
     // Recursively move branches up
     public void Associate(Oper? parent = null)
     {
+        // Absorb trivial
+        for (int i = 0; i < posArgs.Count; i++)
+            posArgs[i] = LegacyForm.Shed(posArgs[i]);
+        for (int i = 0; i < negArgs.Count; i++)
+            negArgs[i] = LegacyForm.Shed(negArgs[i]);
+        
         foreach (Oper o in AllArgs)
             o.Associate(this);
 
@@ -144,6 +161,7 @@ public abstract partial class Oper : IFunction
 
         if (parent is not null)
         {
+            // Associate if types match
             if (parent.GetType() == GetType())
             {
                 if (parent.posArgs.Contains(this))
@@ -190,13 +208,15 @@ public abstract partial class Oper : IFunction
     }
     public virtual Oper Divide(Oper o)
     {
+        if (Like(o))
+            return new Variable(1);
         return new Fraction(new List<Oper> { this }, new List<Oper> { o });
     }
-    public virtual Oper Pow(Oper o)
+    public virtual PowTowRootLog Pow(Oper o)
     {
         return new PowTowRootLog(new List<Oper> { this, o }, new List<Oper> { });
     }
-    public virtual Oper Exp(Oper o)
+    public virtual PowTowRootLog Exp(Oper o)
     {
         return new PowTowRootLog(new List<Oper> { o, this }, new List<Oper> { });
     }
@@ -209,28 +229,89 @@ public abstract partial class Oper : IFunction
         return new PowTowRootLog(new List<Oper> { this }, new List<Oper> { new Variable(1), o });
     }
 
-    public virtual Fraction Factors()
-    {
-        return new Fraction(Copy());
-    }
+    // TODO: in the future, this can have a type form something like Fraction(n->PowTowRootLog, n->PowTowRootLog)
+    public virtual Fraction Factors(){return new Fraction(new PowTowRootLog(new List<Oper>{Copy(), new Variable(1)}, new List<Oper>{}));}
 
-    public Oper CommonFactors(Oper o)
+    public Fraction CommonFactors(Oper o)
     {
+        //Scribe.Info($"\t\tCommon factors of {this} and {o}...");
+        if (Like(o))
+            if (this is Fraction)
+                return (Fraction)Copy();
+            else
+                return new Fraction(Copy());
         OperLike ol = new();
-        Fraction facs = Factors();
-        Fraction oFacs = o.Factors();
+        Fraction facs0 = Factors();
+        Fraction facs1 = o.Factors();
+        List<Oper> basesPos = new();
+        List<Oper> basesNeg = new();
+
+        //Scribe.Info($"\t\tGot factors: {facs0} and {facs1}...");
+
         
-        List<Oper> commonPos = new();
-        foreach (Oper fac in facs.posArgs)
-            if (oFacs.posArgs.Contains(fac, ol))
-                commonPos.Add(fac);
-        List<Oper> commonNeg = new();
-        foreach (Oper fac in facs.negArgs)
-            if (oFacs.negArgs.Contains(fac, ol))
-                commonNeg.Add(fac);
-        if (commonPos.Count == 0)
-            commonPos.Add(new Variable(1));
-        return new Fraction(commonPos, commonNeg);
+        /* Get positive and negative factors from the passed Oper o */
+        foreach (Oper p in facs1.Numerator)
+        {
+            if (p is not PowTowRootLog ptrl)
+                throw Scribe.Issue("Fac was not ptrl");
+            //Scribe.Info($"\t\t\tGot positive base {ptrl.posArgs[0]} from fac {ptrl}");
+            basesPos.Add(ptrl.posArgs[0]);
+        }
+        foreach (Oper p in facs1.Denominator)
+        {
+            if (p is not PowTowRootLog ptrl)
+                throw Scribe.Issue("Fac was not ptrl");
+            //Scribe.Info($"\t\t\tGot negative base {ptrl.posArgs[0]} from fac {ptrl}");
+            basesNeg.Add(ptrl.posArgs[0]);
+        }
+
+        Fraction commonFactors = new();
+        /* Find positive common factors */
+        foreach (Oper facPos in facs0.Numerator)
+        {
+            // TODO: type forms should be able to handle thiss
+            if (facPos is not PowTowRootLog)
+                throw Scribe.Issue($"Factor {facPos} is not a PowTowRootLog!");
+            if (basesPos.Contains(facPos.posArgs[0], ol))
+            {
+                Oper A = facPos.posArgs[1];
+                Oper B = o.Degree(facPos.posArgs[0]);
+                Oper ABsign = new Funcs.Sign(A.Mult(B)); //AB.ReduceOuter();
+                // common factors of x^A and x^B are: Max(AB.sign*A, AB.sign*B) - AB.sign*|A - B|
+                Oper commonFactorExponent = new Funcs.Max(ABsign.Mult(A), ABsign.Mult(B)).Subtract(ABsign.Mult(new Funcs.Abs(A.Subtract(B))));
+                PowTowRootLog commonFactor;
+                if (commonFactorExponent.IsDetermined)
+                    commonFactor = facPos.posArgs[0].Pow(commonFactorExponent.Solution());
+                else
+                    commonFactor = facPos.posArgs[0].Pow(commonFactorExponent);
+                commonFactors.Numerator.Add(commonFactor);
+            }
+        }
+        /* Find positive common factors */
+        foreach (Oper facNeg in facs0.Denominator)
+        {
+            if (facNeg is not PowTowRootLog)
+                throw Scribe.Issue($"Factor {facNeg} is not a PowTowRootLog!");
+            if (basesNeg.Contains(facNeg.posArgs[0], ol))
+            {
+                Oper A = facNeg.posArgs[1];
+                Oper B = new Funcs.Abs(o.Degree(facNeg.posArgs[0]));
+                Oper ABsign = new Funcs.Sign(A.Mult(B)); //AB.ReduceOuter();
+                Oper commonFactorExponent = new Funcs.Max(ABsign.Mult(A), ABsign.Mult(B)).Subtract(ABsign.Mult(new Funcs.Abs(A.Subtract(B))));
+                PowTowRootLog commonFactor;
+                if (commonFactorExponent.IsDetermined)
+                    commonFactor = facNeg.posArgs[0].Pow(commonFactorExponent.Solution());
+                else
+                    commonFactor = facNeg.posArgs[0].Pow(commonFactorExponent);
+                commonFactors.Denominator.Add(commonFactor);
+            }
+        }
+        
+
+        if (commonFactors.Numerator.Count == 0)
+            commonFactors.Numerator.Add(new Variable(1));
+
+        return commonFactors;
     }
 
     public static bool operator <(Oper o0, Oper o1)
