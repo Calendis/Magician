846a5f5be745647d041a178b3489775822a1d2e2
Calendis <calendis1@gmail.com>
2024-01-12 18:22:03 -0700

    basic support for multivalues

---
+++ magician/symbols/base/Multivalue.cs
@@ -0,0 +1,19 @@
+namespace Magician.Symbols;
+using Core;
+
+public class Multivalue : Variable
+{
+    public IVal Principal => principal;
+    public IVal[] Remaining => remaining;
+    readonly IVal principal;
+    readonly IVal[] remaining;
+    public Multivalue(params double[] vs) : this("multivalue", vs.Select(d => new Val(d)).ToArray()) {}
+    public Multivalue(string n, params IVal[] vs) : base(n, vs[0])
+    {
+        if (vs.Length == 0)
+            throw Scribe.Error("Cannot create empty multivalue");
+        principal = vs[0];
+        remaining = vs.Skip(1).ToArray();
+    }
+    // TODO: arithmetic methods for Multivalues
+}
