4baf4a3dd61e1ea8059d27ae062ff97573aba25e
Calendis <calendis1@gmail.com>
2024-01-14 20:39:05 -0700

    fixed awful cosine bug and hacked sine

--- magician/symbols/numeric/Funcs.cs
+++ magician/symbols/numeric/Funcs.cs
@@ -4,11 +4,13 @@ public static class Funcs
 {
     public static double Sin(double x)
     {
+        if (x % Math.PI == 0)
+            return 0;
         return Math.Sin(x);
     }
     public static double Cos(double x)
     {
-        if (x % (Math.PI/2) == 0)
+        if ((x+Math.PI/2) % Math.PI == 0)
             return 0;
         return Math.Cos(x);
     }
