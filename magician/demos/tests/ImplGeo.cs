b1f0c15e0a3e307392ec4f03572be2b99e6a6463
Calendis <calendis1@gmail.com>
2024-01-24 03:49:06 -0700

    Implicit geometry demo

--- magician/demos/tests/ImplGeo.cs
+++ magician/demos/tests/ImplGeo.cs
@@ -4,31 +4,35 @@ using static Alg.Notate;
 using Alg.Symbols;
 using Magician.Geo;
 using Magician.Alg;
+using Magician.Interactive;
 
 public class ImplicitGeom : Spell
 {
+    double walkSpeed = 3.3;
     public override void PreLoop()
     {
         Scribe.Info("Generalized expression rendering...");
 
-        // Expression we want to render
-        //Oper oper = Var("x").Divide(Var("z").Pow(Val(2)).Plus(Val(1)));
-        Equation eq = new(
-        new SumDiff(new ExpLog(new List<Oper>{Var("x"), Val(2)}, new List<Oper>{}), Val(0), new ExpLog(new List<Oper>{Var("y"), Val(2)}, new List<Oper>{}), Val(0), new ExpLog(new List<Oper>{Var("z"), Val(2)}, new List<Oper>{})),
-            Fulcrum.EQUALS,
-            new Fraction(Val(1), Val(0.00000135)).Plus(Val(30000))
-        );
-        SolvedEquation se = eq.Solved();
-
-        // Create geometry from expression
-        Geo.Implicit geo = new(se, 0, 0, 0, 1, (5, 0.01), (5, 0.01));
-        
+        // Expressions we want to render
+        Oper o1 = Var("x").Plus(Var("z").Pow(Val(2))).Root(Val(2));
+        Oper o2 = Val(30).Minus(Var("x").Pow(Val(2))).Minus(Var("z").Pow(Val(2))).Root(Val(2));
+        // Create geometry from expressions
+        Geo.Implicit geo1 = new(o1, -100, -200, 0, 30, 30, (-7, 7, 0.5), (-7, 7, 0.5));
+        Geo.Implicit geo2 = new(o2, 0, 0, 600, 30, 30, (-7, 7, 0.5), (-7, 7, 0.5));
         // Add our geometry to the scene
-        Origin["myGeo"] = geo;
+        Origin["myGeo1"] = geo1.Flagged(DrawMode.OUTER);
+        Origin["myGeo2"] = geo2.Flagged(DrawMode.OUTER);
 
     }
     public override void Loop()
     {
-        //
+        if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_w]){Ref.Perspective.Forward(-walkSpeed);}
+        if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_a]){Ref.Perspective.Strafe(-walkSpeed);}
+        if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_s]){Ref.Perspective.Forward(walkSpeed);}
+        if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_d]){Ref.Perspective.Strafe(walkSpeed);}
+        if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_j]){Ref.Perspective.RotatedY(0.05);}
+        if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_l]){Ref.Perspective.RotatedY(-0.05);}
+        if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_SPACE]){Ref.Perspective.y.Incr(walkSpeed);}
+        if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_LSHIFT]){Ref.Perspective.y.Incr(-walkSpeed);}
     }
 }
