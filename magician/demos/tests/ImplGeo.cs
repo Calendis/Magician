0c047c9b5437a13cb04dd51212d3af9b9ed14835
Calendis <calendis1@gmail.com>
2024-02-01 04:09:32 -0700

    higher res demo

--- magician/demos/tests/ImplGeo.cs
+++ magician/demos/tests/ImplGeo.cs
@@ -18,8 +18,8 @@ public class ImplicitGeom : Spell
         Oper o2 = Val(25).Minus(Var("x").Pow(Val(2))).Minus(Var("z").Pow(Val(2))).Root(Val(2));
         // Create geometry from expressions
         //Geo.Implicit geo1 = new(o1, -100, -200, 0, 30, 30, (-7, 7, 0.5), (-7, 7, 0.5));
-        Geo.Implicit geo2 = new(o2, -900, 0, 600, 160, 160, 1, Sampling.Spiral, (-5, 5, 0.25), (-5, 5, 0.25));
-        Geo.Implicit geo3 = new(o2, 900, 0, 600, 160, 160, 1, null, (-5, 5, 0.25), (-5, 5, 0.25));
+        Geo.Implicit geo2 = new(o2, -900, 0, 600, 160, 160, 1, Sampling.Spiral, (-5, 5, 0.2), (-5, 5, 0.2));
+        Geo.Implicit geo3 = new(o2, 900, 0, 600, 160, 160, 1, null, (-5, 5, 0.2), (-5, 5, 0.2));
         // Add our geometry to the scene
         //Origin["myGeo1"] = geo1.Flagged(DrawMode.OUTER);
         Origin["myGeo2"] = geo2.Flagged(DrawMode.OUTER);
@@ -36,5 +36,6 @@ public class ImplicitGeom : Spell
         if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_l]){Ref.Perspective.RotatedY(-0.05);}
         if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_SPACE]){Ref.Perspective.y.Incr(walkSpeed);}
         if (Events.keys[SDL2.SDL.SDL_Keycode.SDLK_LSHIFT]){Ref.Perspective.y.Incr(-walkSpeed);}
+        //Scribe.Flush();
     }
 }
