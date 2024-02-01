19917f4b4ecd10f73d4907983c8f5cfc0cf835f8
Calendis <calendis1@gmail.com>
2024-02-01 16:38:04 -0700

    improved api

--- magician/geo/Implicit.cs
+++ magician/geo/Implicit.cs
@@ -11,17 +11,41 @@ public class Implicit : Node, IRelation
     protected (double, double)[] range;
     protected double[] resolution;
     protected IRelation relation;
-    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, int domain = 2, Sampling? sampling = null, params (double, double, double)[] rangeResos) : base(x, y, z)
+    //List<Mesh.Region> flatRegions;
+    //Mesh.Region[,] regions;
+    //(Mesh.Region, double[], IVal)[,] regionsArgs;
+    int maxOuts;
+    double inScale;
+    double outScale;
+    int maxSolutions;
+    int[] axes;
+    Sampling? sampling;
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, int domain, int[] axes, Sampling? sampling, params (double, double, double)[] rangeResos) : base(x, y, z)
     {
         relation = r;
-        if (rangeResos.Length != Ins)
-            throw Scribe.Error($"Number of range-resolution pairs did not match length of inputs ({Ins})");
+        //if (rangeResos.Length != Ins)
+        //    throw Scribe.Error($"Number of range-resolution pairs did not match length of inputs ({Ins})");
         range = rangeResos.Select(t => (t.Item1, t.Item2)).ToArray();
         resolution = rangeResos.Select(t => t.Item3).ToArray();
+        maxOuts = domain;
+        this.sampling = sampling;
+        this.inScale = inScale;
+        this.outScale = outScale;
+        maxSolutions = -1;
+        this.axes = axes;
         // For easier control over this, you may use a Symbolic
-        GenNodesAndMesh(inScale, outScale, domain, -1, defaultAxes, sampling);
+        //GenMesh(inScale, outScale, domain, -1, axes, sampling);
+        Refresh();
     }
-    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, double reso, int domain = 2, params (double, double)[] ranges) : this(r, x, y, z, inScale, outScale, domain, null, ranges.Select(r => (r.Item1, r.Item2, reso)).ToArray()) { }
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, int[] axes, Sampling? sampling, params (double, double, double)[] rangeResos) : this(r, x, y, z, inScale, outScale, 2, axes, sampling, rangeResos) { }
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, int domain, int[] axes, params (double, double, double)[] rangeResos) : this(r, x, y, z, inScale, outScale, domain, axes, null, rangeResos) { }
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, int[] axes, params (double, double, double)[] rangeResos) : this(r, x, y, z, inScale, outScale, 2, axes, null, rangeResos) { }
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, int domain, Sampling sampling, params (double, double, double)[] rangeResos) : this(r, x, y, z, inScale, outScale, domain, defaultAxes, sampling, rangeResos) { }
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, Sampling sampling, params (double, double, double)[] rangeResos) : this(r, x, y, z, inScale, outScale, 2, defaultAxes, sampling, rangeResos) { }
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, int domain, params (double, double, double)[] rangeResos) : this(r, x, y, z, outScale, inScale, domain, defaultAxes, null, rangeResos) { }
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, params (double, double, double)[] rangeResos) : this(r, x, y, z, inScale, outScale, 2, defaultAxes, null, rangeResos) { }
+    public Implicit(IRelation r, double x, double y, double z, double scale, params (double, double, double)[] rangeResos) : this(r, x, y, z, scale, scale, 2, defaultAxes, null, rangeResos) { }
+    public Implicit(IRelation r, double x, double y, double z, double inScale, double outScale, int domain, int[] axes, Sampling? sampling, double reso, params (double, double)[] ranges) : this(r, x, y, z, inScale, outScale, domain, axes, sampling, ranges.Select(r => (r.Item1, r.Item2, reso)).ToArray()) { }
 
     public IVal Cache => relation.Cache;
 
@@ -31,7 +55,11 @@ public class Implicit : Node, IRelation
     // maxOuts=1 will only generate real solutions
     // maxOuts=2 will include complex solutions
     // maxouts>2 allows for additional non-arithmetic data to be passed to the generator
-    public void GenNodesAndMesh(double inScale, double outScale, int maxOuts, int maxSolutions, int[] axes, Sampling? sampling=null, Func<double[], Color>? colGen = null)
+    public void Refresh()
+    {
+        GenMesh(GetVals(maxOuts, sampling), (int)((range[0].Item2 - range[0].Item1) / resolution[0]));
+    }
+    public List<(Mesh.Region, double[], IVal)> GetVals(int maxOuts, Sampling? sampling)
     {
         NDCounter solveSpace = new(range, resolution);
         List<(Mesh.Region, double[], IVal)> flatRegArgs = new();
@@ -42,31 +70,34 @@ public class Implicit : Node, IRelation
         // consist of multiple disjoint sections, we need to sort them into separate mesh groups
         do
         {
-            double[] inVals = new double[Ins];
-            for (int i = 0; i < Ins; i++)
+            double[] inVals = new double[Math.Min(2,Ins)];
+            //for (int i = 0; i < Ins; i++)
+            for (int i = 0; i < range.Length; i++)
             {
                 inVals[i] = solveSpace.Get(i);
             }
+            // TODO: Get outVal from the node if it exists
             IVal outVal = EvalCopy(inVals);
 
             if (sampling is not null)
             {
-                IVal tr = sampling.Evaluate(inVals[0]/(range[0].Item2-range[0].Item1), inVals[1]);
+                IVal tr = sampling.Evaluate(inVals[0] / (range[0].Item2 - range[0].Item1), inVals[1]);
                 if (tr.Dims < 2)
                     tr.Push(0);
                 inVals = tr.Values.ToArray();
                 outVal = EvalCopy(inVals);
             }
 
+            // TODO: clarify this magic threshold
             if (outVal.Trim().Dims > maxOuts && outVal.Dims > 1 && Math.Abs(outVal.Get(1)) < 0.001)
                 outVal.Set(1, 0);
-                
+
             if ((outVal.Trim().Dims > maxOuts) || double.IsNaN(outVal.Magnitude) || double.IsInfinity(outVal.Magnitude))
                 flatRegArgs.Add((Mesh.Region.BORDER, inVals, outVal));
             else
                 flatRegArgs.Add((Mesh.Region.UNEXPLORED, inVals, outVal));
             last = flatRegArgs[^1].Item1;
-            lastRow = inVals[1];
+            lastRow = inVals.Length > 1 ? inVals[1] : lastRow;
         } while (!solveSpace.Increment());
 
         int width = (int)((range[0].Item2 - range[0].Item1) / resolution[0]);
@@ -76,8 +107,14 @@ public class Implicit : Node, IRelation
         // Paste the data back in
         for (int i = 0; i < flatRegions.Count; i++)
             flatRegArgs[i] = (flatRegions[i], flatRegArgs[i].Item2, flatRegArgs[i].Item3);
-        // Format the data rectangularly
+        return flatRegArgs;
+    }
+    public void GenMesh(List<(Mesh.Region, double[], IVal)> flatRegArgs, int width, Func<double[], Color>? colGen = null)
+    {
+        // Format the data 
+        List<Mesh.Region> flatRegions = flatRegArgs.Select(t => t.Item1).ToList();
         (Mesh.Region, double[], IVal)[,] regionsArgs = new (Mesh.Region, double[], IVal)[flatRegions.Count / width, width];
+
         Mesh.Region[,] regions = new Mesh.Region[flatRegions.Count / width, width];
         for (int i = 0; i < flatRegions.Count / width; i++)
             for (int j = 0; j < width; j++)
@@ -111,10 +148,13 @@ public class Implicit : Node, IRelation
             }
         }
 
+        //constituents.Clear();
         int brCounter = 0;
+        int ndCounter = 0;
+        int c = 0;
         foreach (List<(double[], IVal)> branch in preMeshes)
         {
-            int c = Count;
+            //int c = Count;
             foreach ((double[] inVals, IVal outVal) in branch)
             {
                 double[] xyz = new double[3];
@@ -134,27 +174,41 @@ public class Implicit : Node, IRelation
                 // Default colouring
                 if (true || colourData.Count == 0)
                 {
-                    double theta = outVal.Trim().Dims == 1 ? outVal.Magnitude : Math.Atan2(outVal.Get(1), outVal.Get()); ;
+                    double colX = outVal.Get();
+                    double colY = outVal.Dims < 2 ? 0 : outVal.Get(1);
+                    colX /= 100;
+                    colY /= 100;
+                    double theta = outVal.Trim().Dims == 1 ? colX : Math.Atan2(colY, colX); ;
+                    
                     if (double.IsNaN(theta) || !double.IsFinite(theta))
-                        theta = outVal.Trim().Dims == 1 ? outVal.Get() < 0 ? Math.PI : 0 : Math.Atan2(outVal.Get(1), outVal.Get());
+                        theta = outVal.Magnitude;
                     hsla[0] = theta;
                     hsla[1] = 1;
                     hsla[2] = 1;
                     hsla[3] = 255;
                 }
 
-                Node n = new(xyz[0], xyz[1], xyz[2], new HSLA(hsla[0], hsla[1], hsla[2], hsla[3]));
-                Add(n);
+                if (Count > ndCounter)
+                    this[ndCounter].To(xyz[0], xyz[1], xyz[2]).Colored(new HSLA(hsla[0], hsla[1], hsla[2], hsla[3]));
+                else
+                {
+                    Node n = new(xyz[0], xyz[1], xyz[2], new HSLA(hsla[0], hsla[1], hsla[2], hsla[3]));
+                    Add(n);
+                }
+                ndCounter++;
             }
             //branchMeshes.Add(Mesh.Rect(width, (int)solveSpace.Max, c));
             //branchMeshes.Add(Mesh.Jagged(jaggeds[i - 1], walls[i - 1] ?? new(), deltOffsets, c));
             branchMeshes.Add(Mesh.Jagged(branchIdcs[brCounter++], regions, c));
+            c = branch.Count;
         }
 
         // Assemble the mesh
         faces = new(branchMeshes.Aggregate(new List<int[]>(), (m, n) => m = m.Concat(n.Faces).ToList()));
+        if (faces.Faces.Count == 0)
+            faces = null;
         //faces = Mesh.Regional(regionalArgs, (int)((range[0].Item2 - range[0].Item1) / resolution[0]));
-        Scribe.Info($"Done regional meshing. Faces: {faces.Faces.Count}, Nodes: {Count}");
+        //Scribe.Info($"Done regional meshing. Faces: {faces.Faces.Count}, Nodes: {Count}");
     }
 
     public IVal Evaluate(params double[] args)
