56870564f30e175f05f7e2d13978b7d0752b774b
Calendis <calendis1@gmail.com>
2024-01-31 17:18:44 -0700

    Implement implicit geometry

--- magician/geo/Implicit.cs
+++ magician/geo/Implicit.cs
@@ -34,9 +34,10 @@ public class Implicit : Node, IRelation
     public void GenNodesAndMesh(double inScale, double outScale, int maxOuts, int maxSolutions, int[] axes, Func<double[], Color>? colGen = null)
     {
         NDCounter solveSpace = new(range, resolution);
-        List<List<(double[], IVal)>> preMeshes = new();
-        // Get all the values out of the function. Because the function may be multivalued, we 
-        // sort them into separate mesh groups
+        List<(Mesh.Region, double[], IVal)> flatRegArgs = new();
+
+        // Get all the values out of the function. Because the function may be multivalued, have holes, or 
+        // consist of multiple disjoint sections, we need to sort them into separate mesh groups
         do
         {
             double[] inVals = new double[Ins];
@@ -44,33 +45,74 @@ public class Implicit : Node, IRelation
             {
                 inVals[i] = solveSpace.Get(i);
             }
-            IVal outVal = Evaluate(inVals);
-
-            int solutions = 1;
-            if (outVal is Multivalue mv)
-            {
-                solutions = mv.Solutions;
-                //Scribe.Info($"{outVal} is Multivalued, with {solutions} solutions");
-            }
-
-            for (int s = 0; s < solutions; s++)
-            {
-                if (s == maxSolutions)
-                    break;
-                if (preMeshes.Count < s + 1) { preMeshes.Add(new()); }
-                // Restrict the outVal
-                if (outVal.Trim().Dims <= maxOuts)
-                    preMeshes[s].Add((inVals, outVal is Multivalue mv2 ? new Val(mv2.All[s]) : outVal));
-            }
-
+            IVal outVal = EvalCopy(inVals);
+        
+            if (outVal.Trim().Dims > maxOuts || double.IsNaN(outVal.Magnitude) || double.IsInfinity(outVal.Magnitude))
+                flatRegArgs.Add((Mesh.Region.BORDER, inVals, outVal));
+            else
+                flatRegArgs.Add((Mesh.Region.UNEXPLORED, inVals, outVal));
         } while (!solveSpace.Increment());
+        //while (!solveSpace.Increment())
+        //{
+        //    double[] inVals = new double[Ins];
+        //    for (int i = 0; i < Ins; i++)
+        //    {
+        //        inVals[i] = solveSpace.Get(i);
+        //    }
+        //    IVal outVal = EvalCopy(inVals);
+        //
+        //    if (outVal.Trim().Dims > maxOuts || double.IsNaN(outVal.Magnitude) || double.IsInfinity(outVal.Magnitude))
+        //        flatRegArgs.Add((Mesh.Region.BORDER, inVals, outVal));
+        //    else
+        //        flatRegArgs.Add((Mesh.Region.UNEXPLORED, inVals, outVal));
+        //}
+
+        int width = (int)((range[0].Item2 - range[0].Item1) / resolution[0]);
+        // Use flood fill to divide the world into regions
+        List<Mesh.Region> flatRegions = flatRegArgs.Select(t => t.Item1).ToList();
+        Mesh.DivideRegions(flatRegions, width);
+        // Paste the data back in
+        for (int i = 0; i < flatRegions.Count; i++)
+            flatRegArgs[i] = (flatRegions[i], flatRegArgs[i].Item2, flatRegArgs[i].Item3);
+        // Format the data rectangularly
+        (Mesh.Region, double[], IVal)[,] regionsArgs = new (Mesh.Region, double[], IVal)[flatRegions.Count/width, width];
+        Mesh.Region[,] regions = new Mesh.Region[flatRegions.Count/width, width];
+        for (int i = 0; i < flatRegions.Count / width; i++)
+            for (int j = 0; j < width; j++)
+            {
+                regionsArgs[i, j] = flatRegArgs[i * width + j];
+                regions[i, j] = regionsArgs[i, j].Item1;
+            }
 
         List<Mesh> branchMeshes = new();
-        
+        List<List<(double[], IVal)>> preMeshes = new(); 
+        List<List<(int, int)>> branchIdcs = new();
+        // Create pre-meshes to account for multiple solutions
+        for (int i = 0; i < flatRegions.Count/width; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                double[] inVals = regionsArgs[i, j].Item2;
+                IVal outVal = regionsArgs[i, j].Item3;
+                int solutions = 1;
+                if (outVal is Multivalue mv)
+                    solutions = mv.Solutions;
+                for (int s = 0; s < solutions; s++)
+                {
+                    if (s == maxSolutions)
+                        break;
+                    if (preMeshes.Count < s + 1) { preMeshes.Add(new()); }
+                    if (branchIdcs.Count < s + 1) { branchIdcs.Add(new()); }
+                    preMeshes[s].Add((inVals, outVal is Multivalue mv2 ? new Val(mv2.All[s]) : outVal));
+                    branchIdcs[s].Add((i, j));
+                }
+            }
+        }
+
+        int brCounter = 0;
         foreach (List<(double[], IVal)> branch in preMeshes)
         {
             int c = Count;
-            int width = (int)((range[0].Item2 - range[0].Item1) / resolution[0]);
             foreach ((double[] inVals, IVal outVal) in branch)
             {
                 double[] xyz = new double[3];
@@ -78,14 +120,15 @@ public class Implicit : Node, IRelation
                 xyz[1] = 0;
                 xyz[2] = 0;
 
-                for (int i = 0; i < Ins; i++) { xyz[axes[i]] = inVals[i] * inScale; }
+                for (int j = 0; j < Ins; j++) { xyz[axes[j]] = inVals[j] * inScale; }
                 xyz[axes[2]] = outVal.Get() * outScale;
+                //xyz[axes[2]] = outVal.Magnitude * outScale;
 
                 // Infer colour data
                 double[] hsla = new double[4];
                 List<double> colourData = outVal.Values.Skip(1).ToList();
-                for (int i = 0; i < Math.Min(4, colourData.Count); i++)
-                    hsla[i] = colourData[i];
+                for (int j = 0; j < Math.Min(4, colourData.Count); j++)
+                    hsla[j] = colourData[j];
                 // Default colouring
                 if (true || colourData.Count == 0)
                 {
@@ -102,15 +145,25 @@ public class Implicit : Node, IRelation
                 Node n = new(xyz[0], xyz[1], xyz[2], new HSLA(hsla[0], hsla[1], hsla[2], hsla[3]));
                 Add(n);
             }
-            branchMeshes.Add(Mesh.Square(width, (int)solveSpace.Max, c));
+            //branchMeshes.Add(Mesh.Rect(width, (int)solveSpace.Max, c));
+            //branchMeshes.Add(Mesh.Jagged(jaggeds[i - 1], walls[i - 1] ?? new(), deltOffsets, c));
+            branchMeshes.Add(Mesh.Jagged(branchIdcs[brCounter++], regions, c));
         }
+
         // Assemble the mesh
         faces = new(branchMeshes.Aggregate(new List<int[]>(), (m, n) => m = m.Concat(n.Faces).ToList()));
+        //faces = Mesh.Regional(regionalArgs, (int)((range[0].Item2 - range[0].Item1) / resolution[0]));
+        Scribe.Info($"Done regional meshing. Faces: {faces.Faces.Count}, Nodes: {Count}");
     }
 
     public IVal Evaluate(params double[] args)
     {
         return relation.Evaluate(args);
+    }
+    IVal EvalCopy(params double[] args)
+    {
+        IVal iv = relation.Evaluate(args);
+        return iv is Multivalue mv ? new Multivalue(mv.All.Select(v => new Val(v)).ToArray()) : new Val(iv);
     }
 }
 
