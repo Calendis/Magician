6a89f57ceca7e13b54499fae9f8ce47de3cbb3d9
Calendis <calendis1@gmail.com>
2024-01-31 17:54:51 -0700

    adjusted impl geo demo

--- magician/demos/tests/ImplGeo.cs
+++ magician/demos/tests/ImplGeo.cs
@@ -18,10 +18,10 @@ public class ImplicitGeom : Spell
         Oper o2 = Val(36).Minus(Var("x").Pow(Val(2))).Minus(Var("z").Pow(Val(2))).Root(Val(2));
         // Create geometry from expressions
         //Geo.Implicit geo1 = new(o1, -100, -200, 0, 30, 30, (-7, 7, 0.5), (-7, 7, 0.5));
-        Geo.Implicit geo2 = new(o2, 0, 0, 600, 160, 160, (-6, 6, 0.25), (-6, 6, 0.25));
+        Geo.Implicit geo2 = new(o2, 0, 0, 600, 160, 160, (-6, 6.25, 0.25), (-6, 6.25, 0.25));
         // Add our geometry to the scene
         //Origin["myGeo1"] = geo1.Flagged(DrawMode.OUTER);
-        Origin["myGeo2"] = geo2.Flagged(DrawMode.INNER);
+        Origin["myGeo2"] = geo2.Flagged(DrawMode.OUTER);
 
     }
     public override void Loop()
