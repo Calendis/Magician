68894cbcccfb1acbf9f04887c1f3ff0e728f68c3
Calendis <calendis1@gmail.com>
2024-08-29 23:08:18 -0600

    plotting tests

--- magician/demos/tests/PlotView.cs
+++ magician/demos/tests/PlotView.cs
@@ -25,19 +25,26 @@ public class EqPlotting : Spell
         if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_D]) { Ref.Perspective.Strafe(walkSpeed); }
         if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_SPACE]) { Ref.Perspective.Lift(walkSpeed); }
         if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_LSHIFT]) { Ref.Perspective.Lift(-walkSpeed); }
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_J]) { Ref.Perspective.RotatedY(0.05); }
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_L]) { Ref.Perspective.RotatedY(-0.05); }
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_K]) { Ref.Perspective.RotatedX(0.05); }
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_I]) { Ref.Perspective.RotatedX(-0.05); }
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_U]) { Ref.Perspective.RotatedZ(0.05); }
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_O]) { Ref.Perspective.RotatedZ(-0.05); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_J]) { Ref.Perspective.RotatedY(0.02); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_L]) { Ref.Perspective.RotatedY(-0.02); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_K]) { Ref.Perspective.RotatedX(0.02); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_I]) { Ref.Perspective.RotatedX(-0.02); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_U]) { Ref.Perspective.RotatedZ(0.02); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_O]) { Ref.Perspective.RotatedZ(-0.02); }
         // shaders :]
         if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_1]) { Shaders.Swap(Shaders.Inverse); }
         if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_0]) { Shaders.Swap(Shaders.Default); }
         // spin the node around :]
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_Z]) { selected!.RotatedY(0.05); }
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_X]) { selected!.RotatedX(0.05); }
-        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_C]) { selected!.RotatedZ(0.05); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_Z]) { selected!.RotatedX(0.0085); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_X]) { selected!.RotatedY(0.0085); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_C]) { selected!.RotatedZ(0.0085); }
+        // info
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_E]) { Scribe.Info($"\n{Origin["myrect"].range}\n{Origin["myrect2"].range}\n{Origin["tanglecube"].range}\n{Origin.range}\n{Origin["bg"][0].range}"); }
+        if (Events.scans[SDL2.SDL.SDL_Scancode.SDL_SCANCODE_F]) {Scribe.Info(Origin["bg"][0]);}
+        
+        Origin["myrect2"].RotatedX(0.0085/6);
+        Origin["myrect2"].RotatedY(0.0085/4);
+        Origin["myrect2"].RotatedZ(0.0085/2);
 
         //Var("parameter").Set(0.5*Math.Sin(Time/2));
         //Origin["myPlot"].Update();
@@ -46,10 +53,14 @@ public class EqPlotting : Spell
     public override void PreLoop()
     {
         Oper sdfTanglecube = Var("x").Pow(Val(4)).Plus(Var("y").Pow(Val(4))).Plus(Var("z").Pow(Val(4))).Divide(Val(2)).Plus(Val(60)).Minus(Var("x").Pow(Val(2)).Plus(Var("y").Pow(Val(2))).Plus(Var("z").Pow(Val(2))).Mult(Val(8)));
-        Implicit sdf = new(sdfTanglecube, 610, 0, -50, 75, 1, new int[] { 0, 2, 1 }, (-5, 5, 0.5), (-5, 5, 0.5), (-5, 5, 0.5));
+        Implicit sdf = new(sdfTanglecube, 610, 0, -50, 75, 1, [0, 2, 1], (-5, 5, 0.6), (-5, 5, 0.6), (-5, 5, 0.6));
         sdf.Flagged(DrawMode.INNER);
         Origin["tanglecube"] = sdf;
-        selected = sdf;
+        selected = Origin["tanglecube"];
+
+        Origin["myrect"] = Create.Rect(-600, 200, 200, 150).Flagged(DrawMode.INNER);
+        Origin["myrect2"] = Create.Star(-300, 200, 20, 150, 100).Colored(HSLA.RandomVisible()).Flagged(DrawMode.FULL | DrawMode.POINTS);
+        //selected = Origin["myrect"];
 
         //Origin["bg"] = new UI.RuledAxes(100, 10, 100, 10).Render();
         //Oper plot = Var("x").Mult(Val(2)).Pow(Val(2)).Plus(Var("z").Mult(Val(3)).Pow(Val(2))).Mult(Var("parameter"));
