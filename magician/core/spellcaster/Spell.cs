236564a5fa5f47368e845f80b85955925503b082
Calendis <calendis1@gmail.com>
2024-08-29 23:07:48 -0600

    handle the cache in spell render

--- magician/core/spellcaster/Spell.cs
+++ magician/core/spellcaster/Spell.cs
@@ -4,6 +4,7 @@
  */
 namespace Magician.Core.Caster;
 using Geo;
+using Magician.Paint;
 using Runes;
 
 public abstract class Spell
@@ -16,11 +17,31 @@ public abstract class Spell
     // The Origin is the root for a tree of Multis
     public Node Origin { get; set; } = Create.Point(null, 0, 0, Runes.Col.UIDefault.FG)
     .Flagged(DrawMode.INVISIBLE)
-    .Tagged("Spell origin")
+    .Tagged("Spell origin")//.Parented(null)  // TODO: bring this back
     ;
 
     // Sets the initial condirions for the Spell. This is called automatically by the Spellcaster
     public abstract void PreLoop();
     // The main loop of the spell. This is called automatically by the spellcaster
     public abstract void Loop();
+
+    internal void InitRenderCache()
+    {
+        //
+    }
+    public void Render()
+    {
+        // Render all object data
+        Ref.Origin.Render(0, 0, 0, 0, 0, 0);
+        // Assign buffer ranges before drawing
+        Paint.Render.CacheRender();
+        // Send stuff to the GPU
+        Renderer.Drawables.DrawAll();
+        Renderer.Drawables.Todo.ps.Clear();
+        Renderer.Drawables.Todo.ls.Clear();
+        Renderer.Drawables.Todo.ts.Clear();
+
+        // SAVE FRAME TO IMAGE
+        //if (Renderer.RControl.saveFrame && frames != stopFrame)
+    }
 }
